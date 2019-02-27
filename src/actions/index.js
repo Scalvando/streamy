62177197bae8796682a433bf1f4409f1e8065ad6
Jared Murray <jared@agileblocks.io>
2019-02-26 23:23:58 -0500

    Added stream list and create button

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM
 } from '../actions/types';
 import streams from '../apis/streams';
+import history from '../history';
 
 export const signIn = userId => {
   return {
@@ -22,10 +23,12 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -41,7 +44,7 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = streams.put(`/streams/${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
